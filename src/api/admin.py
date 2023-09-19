62f65499a56a0939cf519a7f0c6e5f8f87d78946
Rodrigo Alfonsin <ro@roalfonsin.com>
2023-09-19 00:24:02 +0000

    db configured OK

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
-  
+# This file is used to add models to the admin page,
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Office, Package, Reservation
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,9 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Office, db.session))
+    admin.add_view(ModelView(Package, db.session))
+    admin.add_view(ModelView(Reservation, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_verified = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +15,73 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "isVerified": self.is_verified
             # do not serialize the password, its a security breach
+        }
+
+class Office(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    office_type = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Office {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "officeType": self.office_type
+        }
+
+class Package(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, unique=False, nullable=False)
+    total_sessions = db.Column(db.Integer, unique=False, nullable=False)
+    used_sessions = db.Column(db.Integer, unique=False, nullable=False)
+    purchase_date = db.Column(db.DateTime, unique=False, nullable=False)
+    expiration_date = db.Column(db.DateTime, unique=False, nullable=False)
+    is_paid = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Package {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "price": self.price,
+            "totalSessions": self.total_sessions,
+            "usedSessions": self.used_sessions,
+            "purchaseDate": self.purchase_date,
+            "expirationDate": self.expiration_date,
+            "isPaid": self.is_paid,
+            "isActive": self.is_active,
+            "userId": self.user_id
+        }
+    
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time_slot = db.Column(db.Integer, unique=False, nullable=False)
+    type = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.DateTime, unique=False, nullable=False)
+    is_reserved = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_paid = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    office_id = db.Column(db.Integer, db.ForeignKey('office.id'), nullable=False)
+    package_id = db.Column(db.Integer, db.ForeignKey('package.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Reservation {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "timeSlot": self.time_slot,
+            "type": self.type,
+            "date": self.date,
+            "isReserved": self.is_reserved,
+            "isPaid": self.is_paid,
+            "userId": self.user_id,
+            "officeId": self.office_id,
+            "packageId": self.package_id
         }
